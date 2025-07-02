36ea81b3012010404096e930caa44f36fe88c9fb
Lalitha-Kalle <lalithaprogramming@gmail.com>
2025-07-02 12:41:27 +0530

    Subscription model created

---
+++ src/models/subscription.model.js
@@ -0,0 +1,84 @@
+import mongoose from "mongoose";
+
+const subscriptionSchema = new mongoose.Schema({
+  name: {
+    type: String,
+    required: [true, "Subscripion name is required"],
+    trim: true,
+    minLength: 2,
+    maxLength: 100,
+  },
+  price: {
+    type: Number,
+    required: [true, "Subscription price is required"],
+    min: [0, "Price must be greater than 0"]
+  },
+  currency: {
+    type: String,
+    enum: ['INR','USD', 'EUR', 'GBP'],
+    default: 'INR'
+  },
+  frequency: {
+    type: String,
+    enum: ['sports', 'news', 'entertainment', 'lifestyle', 'technology', 'finance', 'politics', 'other'],
+    required: true
+  },
+  paymentMethod: {
+    type: String,
+    required: true,
+    trim: true,
+  },
+  status: {
+    type: String,
+    enum: ['active', 'cancelled', 'expired'],
+    default: 'active'
+  },
+  startDate: {
+    type: Date,
+    required: true,
+    validate : {
+      validator: (value) => value <= new Date(),
+      message: "Start date must be in past"
+    }
+  },
+  renewalDate: {
+    type: Date,
+    required: true,
+    validate: {
+      validator: function (value) {
+        return value > this.startDate
+      },
+      message: "Renewal date must be after start date",
+    }
+  },
+  user: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: "User",
+    required: true,
+    index: true
+  }
+}, {timestamps: true});
+
+
+//autocalculating renewal date
+subscriptionSchema.pre('save', function (next) {
+  if(!this.renewalDate) {
+    const renewalPeriod = {
+      daily: 1,
+      weekly: 7,
+      monthly: 30,
+      yearly: 365,
+    }
+
+    this.renewalDate = new Date(this.startDate);
+    this.renewalDate.setDate(this.renewalDate.getDate() + renewalPeriod[this.frequency])
+  }
+
+  if(this.renewalDate < new Date()) {
+    this.status = 'expired';
+  }
+
+  next();
+})
+
+const Subscripion = mongoose.model("Subscription", subscriptionSchema)
