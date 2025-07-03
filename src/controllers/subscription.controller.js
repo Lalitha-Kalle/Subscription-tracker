93289c6c0a3ff507e2214aee1163127b087bb44c
Lalitha-Kalle <lalithaprogramming@gmail.com>
2025-07-03 11:18:56 +0530

    Added post route to get all subscriptions and modified create subscription controller

--- src/controllers/subscription.controller.js
+++ src/controllers/subscription.controller.js
@@ -1,17 +1,53 @@
 import Subscription from '../models/subscription.model.js'
-
+import { StatusCodes } from 'http-status-codes'
 export const createSubscription = async (req, res, next) =>{
   try {
+
+    const exists = await Subscription.findOne({
+      user: req.user._id
+    })
+
+
+    if(exists) {
+      return res.status(StatusCodes.CONFLICT).json({
+        success: false,
+        message: "Subscription with that user already exists"
+      })
+    }
+
     const subscription = Subscription.create({
       ...req.body,
       user: req.user._id
     })
 
-    res.status(201).json({
+    res.status(StatusCodes.CREATED).json({
       success: true,
       message: "Successfully subscription created",
       data: subscription
     })
+  } catch (error) {
+    next(error);
+  }
+}
+
+export const getUsersSubscriptions = async (req, res, next) => {
+  try {
+    if(req.user.id !== req.params.id) {
+      const error = new Error("You are not owner of this account");
+      error.status = 401;
+      throw error;
+    }
+
+    const subscriptions = await Subscription.find({
+      user: req.params.id
+    });
+
+    res.status(200).json({
+      success: true,
+      message: 'Get all user subscriptions',
+      data: subscriptions
+    })
+
   } catch (error) {
     next(error);
   }
--- src/routes/v1/subscription.routes.js
+++ src/routes/v1/subscription.routes.js
@@ -1,9 +1,11 @@
 import { Router } from "express";
-import { createSubscription } from "../../controllers/subscription.controller.js";
+import { createSubscription, getUsersSubscriptions } from "../../controllers/subscription.controller.js";
 import authorize from '../../middlewares/auth.middleware.js'
 
 const subscriptionRoutes = Router()
 
 subscriptionRoutes.post("/", authorize, createSubscription)
+
+subscriptionRoutes.post("/user/:id", authorize, getUsersSubscriptions)
 
 export default subscriptionRoutes
